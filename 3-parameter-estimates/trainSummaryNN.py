b22aa5c207f27f87ec9eece8a3d7ca68b7ddc99b
Edvard Kjesbu Bahr <edvardkb@stud.ntnu.no>
2026-05-05 20:11:44 +0200

    Added some rough draft of 3param code

--- 3-parameter-estimates/trainSummaryNN.py
+++ 3-parameter-estimates/trainSummaryNN.py
@@ -27,6 +27,8 @@ import torch.nn as nn
 import torch.nn.functional as F
 
 class summaryNN(nn.Module):
+    # NN that takes a summary statistic as input and outputs posterior parameters
+    # for the 3 parameters of the stochastic volatility model (mu, phi, sigma).
 
     def __init__(self, input_dim, hidden_dims=(64, 64), activation=nn.ReLU):
         super().__init__()
@@ -59,3 +61,44 @@ class summaryNN(nn.Module):
         sigma_var = F.softplus(self.sigma_var_head(h)) + 1e-6
 
         return mu_mean, phi_mean, sigma_mean, mu_var, phi_var, sigma_var
+
+
+
+epochs = 100
+
+
+model = summaryNN(input_dim=10, hidden_dims=(64, 64), activation=nn.ReLU)
+optimizer = torch.optim.Adam(model.parameters(), lr=1e-3)
+objective = nn.GaussianNLLLoss(full=True)
+
+
+history = {"train_loss": [], "val_loss": []}
+
+
+for epoch in range(epochs):
+    model.train()
+    optimizer.zero_grad()
+
+    mu_mean, phi_mean, sigma_mean, mu_var, phi_var, sigma_var = model(torch.from_numpy(z))
+
+    loss_mu = objective(mu_mean.squeeze(), torch.from_numpy(mu_true), torch.from_numpy(mu_var))
+    loss_phi = objective(phi_mean.squeeze(), torch.from_numpy(phi), torch.from_numpy(phi_var))
+    loss_sigma = objective(sigma_mean.squeeze(), torch.from_numpy(sigma), torch.from_numpy(sigma_var))
+
+    loss = loss_mu + loss_phi + loss_sigma
+    loss.backward()
+    optimizer.step()
+
+    model.eval()
+
+    with torch.no_grad():
+        mu_mean, phi_mean, sigma_mean, mu_var, phi_var, sigma_var = model(torch.from_numpy(z_val))
+
+        val_loss_mu = objective(mu_mean.squeeze(), torch.from_numpy(mu_true_val), torch.from_numpy(mu_var))
+        val_loss_phi = objective(phi_mean.squeeze(), torch.from_numpy(phi_val), torch.from_numpy(phi_var))
+        val_loss_sigma = objective(sigma_mean.squeeze(), torch.from_numpy(sigma_val), torch.from_numpy(sigma_var))
+
+        val_loss = val_loss_mu + val_loss_phi + val_loss_sigma
+
+    history["train_loss"].append(loss.item())
+    history["val_loss"].append(val_loss.item())
